ceafe2fc1b8336bfc7c485f55b37382f8042a4de
louis <louis.mannevy@ten10.com>
2017-05-10 16:48:10 +0100

    made changes as per review

--- src/test/scala/ten10/collaboration/louis'stuff/Louis_favourite_food_is_yorkshire_puddings.scala
+++ src/test/scala/ten10/collaboration/louis'stuff/Louis_favourite_food_is_yorkshire_puddings.scala
@@ -3,11 +3,6 @@ package ten10.collaboration.louis'stuff
 import ten10.collaboration.utils.BaseFeatureSpec
 
 
-'stuff
-
-/**
-  * Created by louisdmannevy on 10/05/17.
-  */
 class Louis_favourite_food_is_yorkshire_puddings extends BaseFeatureSpec{
 
   When ("Louis Favourite food is yorkshire puddings")
