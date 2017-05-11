de4b5c8f4787447c16eaf9440bd4a7af529b77b0
Roger Souke <roger.souke@digital.hmrc.gov.uk>
2017-05-11 10:31:59 +0100

    update

--- src/test/scala/ten10/collaboration/rogersRepo/ivyanthony.scala
+++ src/test/scala/ten10/collaboration/rogersRepo/ivyanthony.scala
@@ -1,12 +1,19 @@
 package ten10.collaboration.rogersRepo
 
+import ten10.collaboration.utils.BaseFeatureSpec
+
 
 /**
   * Created by roger on 10/05/17.
   */
-class ivyanthony extends  {
+class ivyanthony extends BaseFeatureSpec  {
 
   Given("I am home with Ivy and Anthony")
+  When("I cuddle Anthony")
+  Then("Ivy notices us")
+  And("I don't react")
+  Then("She goes proper mad")
+
 
 
 }
